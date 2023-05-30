5d65f08f99e272f5d7b1b3bb45ee5340220b70ec
orseLv99 <orselv99@gmail.com>
2023-05-30 14:05:52 +0900

    path 수정

--- src/pages/contents/dev/projectCD.tsx
+++ src/pages/contents/dev/projectCD.tsx
@@ -8,10 +8,10 @@ export const ProjectCD = () => {
   const isBrowser = useMediaQuery({ query: '(min-width: 800px)' });
   const [resolution, setResolution] = useState({});
   const { unityProvider, unload } = useUnityContext({
-    loaderUrl: 'dev/projectCD/build.loader.js',
-    dataUrl: 'dev/projectCD/build.data',
-    frameworkUrl: 'dev/projectCD/build.framework.js',
-    codeUrl: 'dev/projectCD/build.wasm',
+    loaderUrl: 'dev/ProjectCD/build.loader.js',
+    dataUrl: 'dev/ProjectCD/build.data',
+    frameworkUrl: 'dev/ProjectCD/build.framework.js',
+    codeUrl: 'dev/ProjectCD/build.wasm',
   });
 
   useEffect(() => {
